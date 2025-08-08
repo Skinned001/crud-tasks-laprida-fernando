a2aa9095eaf3c48d6a4507557a89ea90ed06056f
Laprida <psmathe001@gmail.com>
2025-08-08 14:58:44 -0300

    comienzo a trabajar en los controladores

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 import express from 'express';
+import routerTask from './src/routes/task_routes';
+import routerUser from './src/routes/user_routes';
+import { connectDB } from './src/config/database';
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+app.use("/api",routerTask);
+app.use("/api",routerUser);
 
 app.get('/', (req, res) => {
   res.send('Servidor funcionando!');
